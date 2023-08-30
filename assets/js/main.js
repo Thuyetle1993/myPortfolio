a14f5ef2e44bcde7ccb0d2015425a457ccbca489
Thuyet Le <ledangthuyet@gmail.com>
2023-08-30 13:28:13 +0700

    Chinh lai hieu ung Hero

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,7 +134,25 @@
       backSpeed: 50,
       backDelay: 2000
     });
-  }
+  } 
+  //* Change overlay when hover text
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const highlightTextList = document.querySelectorAll(".highlight-text");
+    const sectionTag = document.getElementById("hero");
+
+    highlightTextList.forEach(function(highlightText) {
+        highlightText.addEventListener("mouseover", function() {
+          sectionTag.classList.add("active");
+            console.log("mouse in");
+        });
+
+        highlightText.addEventListener("mouseout", function() {
+          sectionTag.classList.remove("active");
+            console.log("mouse out");
+        });
+    });
+});
 
   /**
    * Skills animation
@@ -205,36 +223,7 @@
       type: 'bullets',
       clickable: true
     }
-  });
-
-  /**
-   * Testimonials slider
-   */
-  // new Swiper('.testimonials-slider', {
-  //   speed: 600,
-  //   loop: true,
-  //   autoplay: {
-  //     delay: 5000,
-  //     disableOnInteraction: false
-  //   },
-  //   slidesPerView: 'auto',
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     type: 'bullets',
-  //     clickable: true
-  //   },
-  //   breakpoints: {
-  //     320: {
-  //       slidesPerView: 1,
-  //       spaceBetween: 20
-  //     },
-
-  //     1200: {
-  //       slidesPerView: 3,
-  //       spaceBetween: 20
-  //     }
-  //   }
-  // });
+  });  
 
   /**
    * Animation on scroll
