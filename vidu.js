27b9022d02cc84252a9294832eb3b63d18c6b524
Thuyet Le <ledangthuyet@gmail.com>
2023-09-01 09:34:29 +0700

    Edit Skill parts

---
+++ vidu.js
@@ -0,0 +1,12 @@
+var myInfo = {
+    name: 'Son Dang',
+    age: 37,
+    address: 'Hanoi, Vietnam' };
+
+
+    myInfo.email = 'ledangthuyet@gmail.com';
+
+    myInfo['name2'] = 'John';
+    myInfo['age2'] = 25;
+
+console.log(myInfo);
